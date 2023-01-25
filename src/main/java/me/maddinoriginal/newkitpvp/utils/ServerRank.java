7607c216c15316f7c46f80f0d87d85191aa59a05
8grundma <martin.grundmann@studium.uni-hamburg.de>
2023-01-25 17:28:34 +0100

    Erste Version der Server Ranks als enum hinzugefügt.

---
+++ src/main/java/me/maddinoriginal/newkitpvp/utils/ServerRank.java
@@ -0,0 +1,23 @@
+package me.maddinoriginal.newkitpvp.utils;
+
+import org.bukkit.ChatColor;
+
+public enum ServerRank {
+
+    OWNER (ChatColor.DARK_RED + "[OWNER]"),
+    ADMIN (ChatColor.RED + "[ADMIN]"),
+    MODERATOR (ChatColor.DARK_GREEN + "[MOD]"),
+    MVP (ChatColor.AQUA + "[MVP]"),
+    VIP (ChatColor.GREEN + "[VIP]"),
+    DEFAULT (null);
+
+    private String tag;
+
+    ServerRank(String tag) {
+        this.tag = tag;
+    }
+
+    private String getTag() {
+        return tag;
+    }
+}
