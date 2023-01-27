087ff931592d5867cb12ceb9715d523c781db564
8grundma <martin.grundmann@studium.uni-hamburg.de>
2023-01-27 12:31:00 +0100

    Funktionalität für Booster Platten in InteractListener hinzugefügt.

--- src/main/java/me/maddinoriginal/newkitpvp/listeners/InteractListener.java
+++ src/main/java/me/maddinoriginal/newkitpvp/listeners/InteractListener.java
@@ -2,6 +2,9 @@ package me.maddinoriginal.newkitpvp.listeners;
 
 import me.maddinoriginal.newkitpvp.NewKitPvP;
 import me.maddinoriginal.newkitpvp.abilities.items.AbilityItem;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -30,6 +33,22 @@ public class InteractListener implements Listener {
         Player p = e.getPlayer();
         Action action = e.getAction();
         ItemStack heldItem = e.getItem();
+        Block block = e.getClickedBlock();
+
+        //DRUCKPLATTEN BOOST
+        if (action.equals(Action.PHYSICAL)) {
+            e.setCancelled(true);
+            Location loc = p.getLocation();
+            if (block.getType().equals(Material.LIGHT_WEIGHTED_PRESSURE_PLATE)) {
+                loc.setPitch(-44);
+                p.setVelocity(loc.getDirection().multiply(2.1));
+            } else if (block.getType().equals(Material.HEAVY_WEIGHTED_PRESSURE_PLATE)) {
+                loc.setY(loc.getY()+0.2);
+                p.teleport(loc);
+                loc.setPitch(-10);
+                p.setVelocity(loc.getDirection().multiply(3));
+            }
+        }
 
         //return if player does not hold an item to prevent NullPointerException
         if (heldItem == null)
